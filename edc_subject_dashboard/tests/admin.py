7ef8b4382ea4c1ea2ff685c05e42ac9eb2b94a7f
erikvw <ew2789@gmail.com>
2017-11-26 19:26:34 +0200

    minor

--- edc_subject_dashboard/tests/admin.py
+++ edc_subject_dashboard/tests/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.admin import AdminSite as DjangoAdminSite
 
-from .models import SubjectConsent, SubjectVisit, Appointment
+from .models import SubjectLocator, SubjectConsent, SubjectVisit, Appointment
 
 
 class AdminSite(DjangoAdminSite):
@@ -11,5 +11,5 @@ edc_subject_dashboard_admin = AdminSite(name='edc_subject_dashboard_admin')
 
 edc_subject_dashboard_admin.register(Appointment)
 edc_subject_dashboard_admin.register(SubjectConsent)
-# edc_subject_dashboard_admin.register(SubjectLocator)
+edc_subject_dashboard_admin.register(SubjectLocator)
 edc_subject_dashboard_admin.register(SubjectVisit)
