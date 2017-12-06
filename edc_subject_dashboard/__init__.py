8597fba2ad080081cc4af3ad63a70c6ca798d13d
erikvw <ew2789@gmail.com>
2017-12-07 01:46:57 +0200

    move UrlConfig class to edc_dashboard

--- edc_subject_dashboard/__init__.py
+++ edc_subject_dashboard/__init__.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 from .model_wrappers import CrfModelWrapper, AppointmentModelWrapper, AppointmentModelWrapperError
 from .model_wrappers import SubjectVisitModelWrapper
-from .url_config import UrlConfig
 
 name = 'edc_subject_dashboard.middleware.DashboardMiddleware'
 if name not in settings.MIDDLEWARE:
