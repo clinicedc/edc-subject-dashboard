ec41313c698ca13832fd4fa723d96a4748b4390e
erikvw <ew2789@gmail.com>
2017-12-18 15:31:22 +0200

    remove offstudy mixin

--- edc_subject_dashboard/view_mixins/subject_dashboard_view_mixin.py
+++ edc_subject_dashboard/view_mixins/subject_dashboard_view_mixin.py
@@ -1,16 +1,14 @@
-from edc_consent.view_mixins import ConsentViewMixin
-from edc_locator.view_mixins import SubjectLocatorViewMixin
-
 from edc_action_item.view_mixins import ActionItemViewMixin
 from edc_appointment.view_mixins import AppointmentViewMixin
+from edc_consent.view_mixins import ConsentViewMixin
+from edc_locator.view_mixins import SubjectLocatorViewMixin
 from edc_metadata.view_mixins import MetaDataViewMixin
-from edc_offstudy.view_mixins import SubjectOffstudyViewMixin
 from edc_visit_schedule.view_mixins import VisitScheduleViewMixin
 
-from .show_hide_forms_view_mixin import ShowHideViewMixin
-from .registered_subject_view_mixin import RegisteredSubjectViewMixin
-from .subject_visit_view_mixin import SubjectVisitViewMixin
 from ..model_wrappers import ActionItemModelWrapper
+from .registered_subject_view_mixin import RegisteredSubjectViewMixin
+from .show_hide_forms_view_mixin import ShowHideViewMixin
+from .subject_visit_view_mixin import SubjectVisitViewMixin
 
 
 class SubjectDashboardViewMixin(
@@ -21,7 +19,6 @@ class SubjectDashboardViewMixin(
         ActionItemViewMixin,
         SubjectVisitViewMixin,
         VisitScheduleViewMixin,
-        # SubjectOffstudyViewMixin,
         ShowHideViewMixin,
         RegisteredSubjectViewMixin):
 
