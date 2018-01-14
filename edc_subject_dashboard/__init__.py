d0f9d30f235981baf2b70aeb481ab79db136f72b
erikvw <ew2789@gmail.com>
2018-01-15 00:43:48 +0200

    use model wrappers from edc_visit_schedule, fix requisition template

--- edc_subject_dashboard/__init__.py
+++ edc_subject_dashboard/__init__.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 
 from .modeladmin_mixins import ModelAdminSubjectDashboardMixin
-from .model_wrappers import CrfModelWrapper, AppointmentModelWrapper, AppointmentModelWrapperError
+from .model_wrappers import AppointmentModelWrapper, AppointmentModelWrapperError
 from .model_wrappers import SubjectVisitModelWrapper
 
 name = 'edc_subject_dashboard.middleware.DashboardMiddleware'
--- edc_subject_dashboard/model_wrappers/__init__.py
+++ edc_subject_dashboard/model_wrappers/__init__.py
@@ -1,4 +1,3 @@
 from .action_item_model_wrapper import ActionItemModelWrapper
 from .appointment_model_wrapper import AppointmentModelWrapper, AppointmentModelWrapperError
-from .crf_model_wrapper import CrfModelWrapper
 from .subject_visit_model_wrapper import SubjectVisitModelWrapper
