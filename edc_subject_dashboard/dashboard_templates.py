1ca72af091c1f701fe269ed3282098efaeb6c115
erikvw <ew2789@gmail.com>
2018-09-02 13:37:57 -0500

    ensure bootstrap version in template paths

--- edc_subject_dashboard/dashboard_templates.py
+++ edc_subject_dashboard/dashboard_templates.py
@@ -2,6 +2,10 @@
 use settings.DASHBOARD_BASE_TEMPLATES.
 """
 
+from edc_dashboard.utils import insert_bootstrap_version
+
 dashboard_templates = dict(
     edc_subject_dashboard_template=f'edc_subject_dashboard/dashboard.html',
 )
+
+dashboard_templates = insert_bootstrap_version(**dashboard_templates)
