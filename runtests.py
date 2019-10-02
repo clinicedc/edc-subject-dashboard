51f2ff5f35ddcccb13c21add943e0f48d21ed398
erikvw <ew2789@gmail.com>
2019-10-02 16:08:59 -0500

    test setup

--- runtests.py
+++ runtests.py
@@ -43,6 +43,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "edc_identifier.apps.AppConfig",
         "edc_visit_schedule.apps.AppConfig",
         "edc_visit_tracking.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
         "edc_subject_dashboard.apps.EdcAppointmentAppConfig",
         "edc_subject_dashboard.apps.AppConfig",
     ],
