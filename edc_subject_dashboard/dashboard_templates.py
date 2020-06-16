0a3abddb3873b999d3e9ab75f689628b502e4ccc
erikvw <ew2789@gmail.com>
2020-06-15 19:03:44 -0500

    minor improvements to dashboard

--- edc_subject_dashboard/dashboard_templates.py
+++ edc_subject_dashboard/dashboard_templates.py
@@ -5,7 +5,7 @@ use settings.DASHBOARD_BASE_TEMPLATES.
 from edc_dashboard.utils import insert_bootstrap_version
 
 dashboard_templates = dict(
-    edc_subject_dashboard_template=f"edc_subject_dashboard/dashboard.html"
+    edc_subject_dashboard_template="edc_subject_dashboard/dashboard.html"
 )
 
 dashboard_templates = insert_bootstrap_version(**dashboard_templates)
