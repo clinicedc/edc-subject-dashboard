cbbd886c6d9e0f62b4538675436f0b98b3341a7a
erikvw <ew2789@gmail.com>
2022-05-02 19:07:18 -0500

    build, lint

--- runtests.py
+++ runtests.py
@@ -41,6 +41,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "edc_dashboard.apps.AppConfig",
         "edc_device.apps.AppConfig",
         "edc_data_manager.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
         "edc_lab.apps.AppConfig",
         "edc_locator.apps.AppConfig",
         "edc_metadata.apps.AppConfig",
@@ -64,7 +65,9 @@ def main():
     if not settings.configured:
         settings.configure(**DEFAULT_SETTINGS)
     django.setup()
-    failures = DiscoverRunner(failfast=True).run_tests([f"{app_name}.tests"])
+    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
+    failfast = any([True for t in sys.argv if t.startswith("--failfast")])
+    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests([f"{app_name}.tests"])
     sys.exit(failures)
 
 
