db6c87f879b4c68cc0834ca4cfc73281aadd5bab
erikvw <ew2789@gmail.com>
2023-10-09 11:56:45 -0500

    update paths from edc-visit-schedule, setup

--- edc_subject_dashboard/tests/tests/test_view_mixins.py
+++ edc_subject_dashboard/tests/tests/test_view_mixins.py
@@ -9,7 +9,7 @@ from edc_locator.view_mixins import (
 from edc_reference import site_reference_configs
 from edc_registration.models import RegisteredSubject
 from edc_utils import get_utcnow
-from edc_visit_schedule import site_visit_schedules
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from edc_visit_tracking.constants import SCHEDULED
 from edc_visit_tracking.models import SubjectVisit
 
